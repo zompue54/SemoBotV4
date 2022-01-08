83d0c0d04b9b6c43f2f640c878bc7091e69ee56f
CEHunter30 <96426754+CEHunter30@users.noreply.github.com>
2022-01-08 12:09:23 +0300

    🦊

---
+++ plugins/pgit.js
@@ -0,0 +1,32 @@
+const Fox = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Fox.addCommand({pattern: 'git', fromMe: true, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+    r_text[0] = "https://bit.ly/3pFQeWS";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `◄━⦁🦊 FOX🦊⦁━►
+            
+
+
+*github* : https://github.com/CEHunter30/FOXIT.
+
+
+◄━⦁🦊 FOX🦊⦁━►
+■□ ~CEHUNTER~■□ 
+
+`}) 
+
+}));
